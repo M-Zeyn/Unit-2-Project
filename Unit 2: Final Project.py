432be2c36bd498acc266ba8318aee42d1f6869d7
M-Zeyn <muhammadzeyn.ebrahim@thekaustschool.org>
2024-11-26 09:06:10 +0300

    I imported time, greeted the user, explained the rules, and created the klist that will have the questions

---
+++ Unit 2: Final Project.py
@@ -0,0 +1,24 @@
+#import random and time
+import random
+import time
+
+#greet the user
+name = input("Enter your name: ")
+print("HELLO",name.upper(),"!")
+
+#explain the rules of the quiz
+print("This quiz was made to raise awareness about SDG #4: Quality Education")
+time.sleep(3)
+print("There are 10 questions")
+time.sleep(2)
+print("If you get a question right you will gain 50 points ")
+time.sleep(4)
+print("If you get a question wrong you will lose 25 points")
+time.sleep(4)
+print("Try to get the highest score")
+time.sleep(1)
+print("Goodluck")
+
+#create list of questions
+questions_list = [""]
+
